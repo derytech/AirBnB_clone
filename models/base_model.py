c9aa39d977ef026275b32308c8e97e7d493bbe5a
derytech <samuelaanyemdery@gmail.com>
2026-09-10 00:44:26 +0000

    Add BaseModel structure

---
+++ models/base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines common attributes and methods for other classes."""
